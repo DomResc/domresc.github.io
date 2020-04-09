c225cda803b9a90a3d38f2838c4f8c403c15df53
Domenico Rescigno <domenico.rescigno@gmail.com>
2020-04-09 21:46:37 +0200

    Aggiunta la pagina blog

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,14 +1,36 @@
 <template>
   <q-page>
-    <div class="text-center">
-      <!-- content -->
-      <p>Blog</p>
-    </div>
+    <q-timeline class="q-pa-xl">
+      <q-timeline-entry title="Event Title" subtitle="February 22, 1986">
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </q-timeline-entry>
+    </q-timeline>
   </q-page>
 </template>
 
 <script>
+const Meed = require("meed");
+const fetch = require("node-fetch");
+const feed = new Meed({ fetch });
+
 export default {
-  // name: 'PageName',
+  name: "Blog",
+  mounted() {
+    (async () => {
+      const user = await feed.user("mcastimovies");
+      console.log(user);
+    })();
+  },
+  data() {
+    return {};
+  }
 };
 </script>
